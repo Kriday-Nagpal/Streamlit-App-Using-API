f1217c496943c63766b288b341c692d9fe06cc59
Kriday-Nagpal <nj0166217@gmail.com>
2026-06-24 19:51:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import requests
 import pandas as pd
 import streamlit as st
 
-st.set_page_config(page_title="Country Info App")
-
 st.title("🌍 Country Info App")
 
 country = st.text_input("Enter Country", "India")
@@ -16,18 +14,24 @@ if st.button("Search"):
 
     if response.status_code == 200:
 
-        data = response.json()[0]
+        data = response.json()
 
-        country_info = [{
-            "Country": data["name"]["common"],
-            "Capital": data["capital"][0],
-            "Region": data["region"],
-            "Population": data["population"]
-        }]
+        if len(data) > 0:
 
-        df = pd.DataFrame(country_info)
+            country_data = data[0]
 
-        st.dataframe(df)
+            info = [{
+                "Country": country_data["name"]["common"],
+                "Region": country_data["region"],
+                "Population": country_data["population"]
+            }]
+
+            df = pd.DataFrame(info)
+
+            st.dataframe(df)
+
+        else:
+            st.error("No country found")
 
     else:
         st.error("Country not found")
